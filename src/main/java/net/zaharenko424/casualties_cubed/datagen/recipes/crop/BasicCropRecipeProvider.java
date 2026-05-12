d5bdaf6a6019e3231b3b70140f9fd4a415af6a6a
Foxyas <CarlosDeck9474@gmail.com>
2026-05-11 22:13:21 -0300

    Update BasicCropRecipeProvider.java

--- src/main/java/net/zaharenko424/casualties_cubed/datagen/recipes/crop/BasicCropRecipeProvider.java
+++ src/main/java/net/zaharenko424/casualties_cubed/datagen/recipes/crop/BasicCropRecipeProvider.java
@@ -8,6 +8,8 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.zaharenko424.casualties_cubed.CasualtiesCubed;
+import net.zaharenko424.casualties_cubed.blocks.GlowFruitBushBlock;
+import net.zaharenko424.casualties_cubed.registry.ModBlocks;
 import net.zaharenko424.casualties_cubed.registry.ModItems;
 
 import java.util.List;
@@ -19,12 +21,13 @@ public class BasicCropRecipeProvider {
         // Example: Sweet Berries with age phases
 
         // 1. Define the individual states (ages 0, 1, and 2)
-        DisplayState age0 = new SimpleDisplayState(Blocks.SWEET_BERRY_BUSH.defaultBlockState().setValue(SweetBerryBushBlock.AGE, 0));
-        DisplayState age1 = new SimpleDisplayState(Blocks.SWEET_BERRY_BUSH.defaultBlockState().setValue(SweetBerryBushBlock.AGE, 1));
-        DisplayState age2 = new SimpleDisplayState(Blocks.SWEET_BERRY_BUSH.defaultBlockState().setValue(SweetBerryBushBlock.AGE, 2));
+        DisplayState age0 = new SimpleDisplayState(ModBlocks.GLOW_FRUIT_BUSH.get().defaultBlockState().setValue(GlowFruitBushBlock.AGE, 0));
+        DisplayState age1 = new SimpleDisplayState(ModBlocks.GLOW_FRUIT_BUSH.get().defaultBlockState().setValue(GlowFruitBushBlock.AGE, 1));
+        DisplayState age2 = new SimpleDisplayState(ModBlocks.GLOW_FRUIT_BUSH.get().defaultBlockState().setValue(GlowFruitBushBlock.AGE, 2));
+        DisplayState age3 = new SimpleDisplayState(ModBlocks.GLOW_FRUIT_BUSH.get().defaultBlockState().setValue(GlowFruitBushBlock.AGE, 3));
 
         // 2. Wrap them in a Transitional state
-        DisplayState transitionalDisplay = new TransitionalDisplayState(List.of(age0, age1, age2));
+        DisplayState transitionalDisplay = new TransitionalDisplayState(List.of(age0, age1, age2, age3));
 
         // 3. Build and save the recipe
         new CropRecipeBuilder(Ingredient.of(ModItems.GLOW_FRUIT.get()))
